70a7fa50bd6e56902a69798170ce00931a747cac
unknown <calebmoses1111@gmail.com>
2018-11-07 09:58:06 +0100

    sportblog

--- sport_blog.php
+++ sport_blog.php
@@ -1,7 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<title>Super_Blog</title>
+	center><h1><b style="color:white ">Welcom to my super blog</b></h1></center>
+<nav>   
+			 <ul>
+		     	<li><a href="home.php" >Home</a></li>
+		     	<li><a href="sign-up.php">sign_up</a></li>
+		    	 <li><a href="log-in.php">log_in</a></li>    
+			 </ul>
+		</nav></br></br>
 </head>
 <body>
 <textarea name="message" rows="8" cols="45"> Votre message ici.
