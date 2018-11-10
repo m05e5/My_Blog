904b93940ac39eb3b53b28a325a0cc849e3e519a
unknown <calebmoses1111@gmail.com>
2018-11-10 23:06:12 +0100

    coding

--- newsblog.php
+++ newsblog.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<title>Super_Blog</title>
+	<center><h1><b style="color:black ">Welcom to my super blog</b></h1></center>
+<nav>   
+			 <ul>
+		     	<li><a href="home.php" >Home</a></li>
+		     	<li><a href="sign-up.php">sign_up</a></li>
+		    	 <li><a href="log-in.php">log_in</a></li>    
+			 </ul>
+		</nav></br></br>
 </head>
 <body>
-
+<textarea name="message" rows="10" cols="50"> Votre message ici.</textarea>
 </body>
 </html>
