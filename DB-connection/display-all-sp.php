64514cead2028fa31242b3efa882fec63e4c5db8
unknown <calebmoses1111@gmail.com>
2018-11-20 08:44:53 +0100

    arranging

---
+++ DB-connection/display-all-sp.php
@@ -0,0 +1,8 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$DBname = "my_blog";
+
+// Create connection
+	$conn =new mysqli($servername, $username, $password, $DBname);
