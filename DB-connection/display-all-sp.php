5f0ff5285940fe57899adf2e623579001f7f7dc4
unknown <calebmoses1111@gmail.com>
2018-11-20 08:51:23 +0100

    arranging

--- DB-connection/display-all-sp.php
+++ DB-connection/display-all-sp.php
@@ -15,3 +15,12 @@
 
 	$sql = "SELECT Email, password FROM users";
 	$result = $conn->query($sql);
+if ($result->num_rows >0){
+		//output data of each row
+		while ($row = $result->fetch_assoc()) {
+			if ($row['Email']==$_POST['Email']) {
+				if ($row['password']==$_POST['password']) {
+					include('../blog.php');
+				}
+			}
+		}
