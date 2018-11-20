f6bf19f9e01e5b8a0800121c598d52f99aab47eb
unknown <calebmoses1111@gmail.com>
2018-11-20 08:50:50 +0100

    arranging

--- DB-connection/display-all-sp.php
+++ DB-connection/display-all-sp.php
@@ -6,3 +6,12 @@
 
 // Create connection
 	$conn =new mysqli($servername, $username, $password, $DBname);
+
+// Check connection
+	if ($conn->connect_error) 
+	{
+    	die("Connection failed: " . $conn->connect_error);
+	}  
+
+	$sql = "SELECT Email, password FROM users";
+	$result = $conn->query($sql);
