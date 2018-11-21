208bdeaa6c87d537a624bb0974a2e9bd54f6dc9b
unknown <calebmoses1111@gmail.com>
2018-11-21 21:57:18 +0100

    linking

--- newsconnect.php
+++ newsconnect.php
@@ -3,3 +3,11 @@
 	$username = "root";
 	$password = "";
 	$DBname="my_blog";
+// Create connection
+	$conn =new mysqli($servername, $username, $password, $DBname);
+
+	// Check connection
+	if ($conn->connect_error) 
+	{
+    	die("Connection failed: " . $conn->connect_error);
+	}  
