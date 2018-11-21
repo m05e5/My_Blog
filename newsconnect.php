e50c0050ee107ebc105ff7a47c7af9e9f45c80be
unknown <calebmoses1111@gmail.com>
2018-11-21 21:57:40 +0100

    linking

--- newsconnect.php
+++ newsconnect.php
@@ -10,4 +10,15 @@
 	if ($conn->connect_error) 
 	{
     	die("Connection failed: " . $conn->connect_error);
-	}  
+	}  $sql = "INSERT INTO spblog (comment) VALUES ('".$_POST['message']."')";
+	
+		if ($conn->query($sql) === TRUE) 
+		{
+    		echo $_POST['message'];
+    		 ?>
+    		<form method="post" action="sport_blog.php">
+    		</br><input type="submit" value="Back to my blog" />
+			</form>	<?php
+		} else {
+    	echo "Error: " . $sql . "<br>" . $conn->error;
+		}
