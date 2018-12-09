335b4f106d7fc853443b3f92139372c4a630f231
unknown <calebmoses1111@gmail.com>
2018-12-09 23:22:54 +0100

    defultblog

--- defultblog.php
+++ defultblog.php
@@ -1,3 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+	<title>Super_Blog</title>
+	<center><h1><b style="color:black ">Welcome to my super blog</b></h1></center>
+<nav>   
+			 <ul>
+		     	<li><a href="home.php" >Home</a></li>
+		     	<li><a href="sign-up.php">sign_up</a></li>
+		    	 <li><a href="log-in.php">log_in</a></li>    
+			 </ul>
+		</nav>
