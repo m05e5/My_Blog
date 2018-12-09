b72fc3b236e83a535664786a3364c7f4e9354a0d
unknown <calebmoses1111@gmail.com>
2018-12-09 23:23:42 +0100

    defultblog

--- defultblog.php
+++ defultblog.php
@@ -10,4 +10,13 @@
 		     	<li><a href="sign-up.php">sign_up</a></li>
 		    	 <li><a href="log-in.php">log_in</a></li>    
 			 </ul>
-		</nav>
+		</nav></br></br>
+</head>
+<body>
+	<p>here you can write all the news you like about medecine</p>
+<form method="post" action="medconnect.php">
+	<textarea name="message" rows="20" cols="50"> Votre message ici.
+  	</textarea>
+  </br>
+  <input type="submit" value="save" />
+</form>
