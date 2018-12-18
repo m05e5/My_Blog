2348c0c2c82b2377bd6d675284fd9e8c73c36599
unknown <calebmoses1111@gmail.com>
2018-12-18 20:41:34 +0100

    arranging

--- home.php
+++ home.php
@@ -19,35 +19,40 @@
           padding: 2px; 
         }
 			
-div.gallery {
-    margin: 5px;
-    border: 1px solid #ccc;
-    float: left;
-    width: 295px;
-    background-color:rgb(255, 255, 255); 
-}
+      div.gallery 
+        {
+          margin: 5px;
+          border: 1px solid #ccc;
+          float: left;
+          width: 295px;
+          background-color:rgb(255, 255, 255); 
+        }
 
-div.gallery:hover {
-    border: 1px solid #777;
-    background-color:rgb(0, 255, 255);
-}
+      div.gallery:hover
+        {
+          border: 1px solid #777;
+          background-color:rgb(0, 255, 255);
+        }   
 
-div.gallery img {
-    width: 285px;
-    height:250px;
-    border-radius: 15%;
-}
+      div.gallery img
+        {
+          width: 285px;
+          height:250px;
+          border-radius: 15%;
+        }
 
-div.desc {
-    padding: 15px;
-    text-align: center;
-}
-div.bor {
-    margin: 2px;
-    border: 1px solid #ccc;
-    float: left;
-    background-color:rgb(0, 255, 55); 
-
+      div.desc 
+        {
+          padding: 15px;
+          text-align: center;
+        }
+      div.bor 
+        {
+          margin: 2px;
+          border: 1px solid #ccc;
+          float: left;
+          background-color:rgb(0, 255, 55); 
+        }
 		</style>
     <link rel="stylesheet" type="text/css" href="css/index.css">
     <title>
@@ -58,45 +63,71 @@ div.bor {
 </head>
 <body>
 
-<center><h1><b style="color:rgb(0, 0, 0) ">Welcom to my super blog</b></h1></center>
-<nav>   
-			 <ul>
-		     	<li><a style="color: white;" href="home.php" >Home</a></li>
-		     	<li><a href="sign-up.php">sign_up</a></li>
-		    	 <li><a href="log-in.php">log_in</a></li>    
-			 </ul>
-		</nav></br></br>
+  <center> 
+    <h1>
+      <b style="color:rgb(0, 0, 0) ">
+        Welcom to my super blog
+      </b>
+    </h1>
+  </center>
+  <nav>   
+			<ul>
+		     	<li>
+            <a style="color: white;" href="home.php" >
+              Home
+            </a>
+          </li>
+		     	<li>
+            <a href="sign-up.php">
+            sign_up
+          </a>
+        </li>
+		    <li>
+          <a href="log-in.php">
+            log_in
+          </a>
+        </li>    
+			</ul>
+	</nav></br></br>
 
 </head>
 <body>
-<div class="bor">
-<div class="gallery">
-  <a target="_blank" href="log-in.php">
-    <img src="sp.jpg" alt="Cinque Terre" width="250" height="200">
-  </a>
-  <div class="desc">Here you can create a sport blog. on this blog you can write all what you like aboute sport</div>
-</div>
+  <div class="bor">
+    <div class="gallery">
+      <a target="_blank" href="log-in.php">
+        <img src="sp.jpg" alt="Cinque Terre" width="250" height="200">
+      </a>
+      <div class="desc">
+        Here you can create a sport blog. on this blog you can write all what you like aboute sport
+      </div>
+    </div>
 
-<div class="gallery">
-  <a target="_blank" href="log-in.php">
-    <img src="med.jpg" alt="Forest" width="250" height="200">
-  </a>
-  <div class="desc">Here you can create a Medecinal blog. on this blog you can write all what you like aboute drugs, medecins, hospitals ...</div>
-</div>
+    <div class="gallery">
+      <a target="_blank" href="log-in.php">
+        <img src="med.jpg" alt="Forest" width="250" height="200">
+      </a>
+      <div class="desc">
+        Here you can create a Medecinal blog. on this blog you can write all what you like aboute drugs, medecins, hospitals ...
+      </div>
+    </div>
 
-<div class="gallery">
-  <a target="_blank" href="log-in.php">
-    <img src="news.jpg" alt="Northern Lights" width="250" height="200">
-  </a>
-  <div class="desc">Here you can create a news blog.  on this blog you can write all what you like aboute the news in your locality(school, city, quater, country, ...</div>
-</div>
+    <div class="gallery">
+      <a target="_blank" href="log-in.php">
+        <img src="news.jpg" alt="Northern Lights" width="250" height="200">
+      </a>
+      <div class="desc"> 
+        Here you can create a news blog.  on this blog you can write all what you like aboute the news in your locality(school, city, quater, country, ...
+      </div>
+    </div>
 
-<div class="gallery">
-  <a target="_blank" href="log-in.php">
-    <img src="mo.jpg" alt="Mountains" width="250" height="200">
-  </a>
-  <div class="desc">Here you can create a sport blog.  on this blog you can write all what you like</div>
-</div>
+    <div class="gallery">
+      <a target="_blank" href="log-in.php">
+        <img src="mo.jpg" alt="Mountains" width="250" height="200">
+      </a>
+      <div class="desc">
+        Here you can create a sport blog.  on this blog you can write all what you like
+      </div>
+    </div>
 	</div>
 </body>
 </html>
