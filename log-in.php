c696aabbfb466cd826a75ec556168de30f1f9f9d
unknown <calebmoses1111@gmail.com>
2018-12-18 20:16:37 +0100

    style

--- log-in.php
+++ log-in.php
@@ -1,43 +1,66 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<div class="background-image"><style>
-			body 
-			{
-				background-image: url('im.JPG');
-				background-size: cover;
+		<div class="background-image">
+			<style>
+				body 
+					{
+					background-image: url('im.JPG');
+					background-size: cover;
 				
-			}
-			 .li {
-    			text-align: right;	
-    			text-decoration: none;
-    			color: white;
-    			border: 1px solid blue;
-    			background: blue;
-    			font-weight: bold;	
-    			padding: 2px;	
-    		}
+					}
+			 	.li {
+    				text-align: right;	
+    				text-decoration: none;
+    				color: white;
+    				border: 1px solid blue;
+    				background: blue;
+    				font-weight: bold;	
+    				padding: 2px;	
+    				}
 
 
-		</style>
-	</div>
+			</style>
+		</div>
 
 		<link rel="stylesheet" type="text/css" href="css/index.css">
-		<title>Super_Blog</title>
+		<title>	
+			Super_Blog
+		</title>
 		<a class='li' href="log-in.php">Log In</a>
 		<a class='li' href="sign-up.php">Sign In</a>
 	</head>
 	<body>
-		<center><h1><b>Welcom to my super blog</b></h1></center>
+		<center>	
+			<h1>
+				<b>
+					Welcom to my super blog
+				</b>
+			</h1>
+		</center>
 		<nav>   
 				 <ul>  
-		    	 	<li><a href="Home.php">Home</a></li> 
-		     		<li><a href="sign-up.php">sign_up</a></li>
-			    	 <li><a style="color: white;" href="log-in.php">log_in</a></li>    
+		    	 	<li>
+		    	 		<a href="Home.php">Home</a>
+		    	 	</li> 
+		     		<li>
+		     			<a href="sign-up.php">sign_up</a>
+		     		</li>
+			    	<li>
+			    	 	<a style="color: white;" href="log-in.php">log_in</a>
+			    	</li>    
 				 </ul>
 		</nav>
 		<p>
-			<center><h1><b><i>LOG_IN</i></b></h1></center>
+			<center>
+				<h1>
+					<b>
+						<i>
+							LOG_IN
+						</i>
+					</b>
+				</h1>
+			</center>
 			<section>	                    
 		        <form method="POST" action="DB-connection/collect.php">
 		             <center><input type="Email" name="Email" id="Email" placeholder="Email" size="70" maxlength="10" /></br></br></center>
@@ -50,4 +73,4 @@
   			</section>
 		</p>
 </body>
-</html>
+</html>s
