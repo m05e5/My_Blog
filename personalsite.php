0d7970c778ca09388c979896ac59bd5b9f775749
unknown <calebmoses1111@gmail.com>
2019-01-04 23:45:48 +0100

    personalise

--- personalsite.php
+++ personalsite.php
@@ -3,3 +3,10 @@
 	$username = "root";
 	$password = "";
 	$DBname="my_blog";
+
+		$conn =new mysqli($servername, $username, $password, $DBname);
+		// Check connection
+	if ($conn->connect_error) 
+	{
+    	die("Connection failed: " . $conn->connect_error);
+	}  
