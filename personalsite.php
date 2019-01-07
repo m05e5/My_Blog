472e00ac48a62f2953889ce6d5c8ed817be639a9
unknown <calebmoses1111@gmail.com>
2019-01-07 09:02:42 +0100

    trying to atribute an account to every user

--- personalsite.php
+++ personalsite.php
@@ -2,24 +2,33 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$DBname="my_blog";
+	$DBname = "my_blog";
 
-		$conn =new mysqli($servername, $username, $password, $DBname);
-		// Check connection
+// Create connection
+	$conn =new mysqli($servername, $username, $password, $DBname);
+
+	// Check connection
 	if ($conn->connect_error) 
 	{
     	die("Connection failed: " . $conn->connect_error);
 	}  
-	if ($_POST['password']===$_POST['check_password']) 
-	{
-		$sql = "INSERT INTO users (User_name, Favoritetopic, password, Email) VALUES ('".$_POST['Name']."', '".$_POST['Favorite']."', '".$_POST['password']."', '".$_POST['Email']."')";
-	
-		if ($conn->query($sql) === TRUE) 
-		{
-    		echo "New record created successfully";
-		} else {
-    	echo "Error: " . $sql . "<br>" . $conn->error;
+
+	$sql = "SELECT Email, password FROM users";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows >0){
+		//output data of each row
+		while ($row = $result->fetch_assoc()) {
+			if ($row['Email']==$_POST['Email']) {
+				if ($row['password']==$_POST['password']) {
+					include('../blog.php');
+				}
+			}
 		}
+		} else {
+    	echo "0 result";
+		
 	}
-$conn->close();
+mysqli_close($conn);
 ?>
+ 
