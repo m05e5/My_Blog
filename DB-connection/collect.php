db52ade694abd0b80b548a8b8c9a7b4de34e0ba8
unknown <calebmoses1111@gmail.com>
2019-01-10 12:01:01 +0100

    work

--- DB-connection/collect.php
+++ DB-connection/collect.php
@@ -7,26 +7,40 @@
 	$DBname = "my_blog";
 
 // Create connection
-	$conn =new mysqli($servername, $username, $password, $DBname);
+//conn =new mysqli($servername, $username, $password, $DBname);
+
+
+
+	try
+	{
+		$conn = new PDO('mysql:host=localhost;dbname=my_blog;charset=utf8', 'root', '');
+	}catch(Exeption $e)
+	{
+		die($e->getMessage());
+	}
+
+
+
+
 
 	// Check connection
-	if ($conn->connect_error) 
-	{
-    	die("Connection failed: " . $conn->connect_error);
-	}  
+
 
 	$sql = "SELECT Email, password FROM users";
 	$result = $conn->query($sql);
 
-	if ($result->num_rows >0){
 		//output data of each row
-		while ($row = $result->fetch_assoc()) {
+		while ($row = $result->fetch()) {
 			if ($row['Email']==$_POST['Email']) {
 				if ($row['password']==$_POST['password']) {
 
 					$req = $conn->prepare('SELECT id FROM  users WHERE Email = ?');
-					$res = $req->execute(array($row['Email']));
-					echo $row['Email'];
+					$req->execute(array($row['Email']));
+					$idUser = $req->fetch();
+
+					echo $idUser['id'];
+
+					
 
 	
 					
@@ -34,9 +48,5 @@
 				}
 			}
 		}
-		} else {
-    	echo "0 result";
-		
-	}
-mysqli_close($conn);
+
 ?>
