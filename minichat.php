e0638df1f4a7a6213923c48d9d83beb591c559ce
unknown <calebmoses1111@gmail.com>
2019-01-10 15:18:31 +0100

    I am trying to create a session for each user. in such a way that every user will have his personall account

---
+++ minichat.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+  <head>
+     <title>Mini-chat</title>
+            <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+  </head>
+  <style type="text/css">
+      form
+          	{
+            text-align:center;    
+  			}    
+  </style>    
+  <body> 
