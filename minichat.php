40dc333d21bc860357af7ba0bb9270892a6c3e66
unknown <calebmoses1111@gmail.com>
2019-01-10 15:19:56 +0100

    I am trying to create a session for each user. in such a way that every user will have his personall account

--- minichat.php
+++ minichat.php
@@ -10,4 +10,12 @@
             text-align:center;    
   			}    
   </style>    
-  <body> 
+  <body>
+  	 <form action="minichat_post.php" method="post">
+        <p>        
+        	<label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
+   	        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
+        	<input type="submit" value="Envoyer" /> 
+        </p>    
+    </form>
+ 
