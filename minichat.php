150819c0f03f4373bfd2b85da146df57ed264ed3
unknown <calebmoses1111@gmail.com>
2019-01-10 15:25:53 +0100

    I am trying to create a session for each user. in such a way that every user will have his personall account

--- minichat.php
+++ minichat.php
@@ -18,4 +18,21 @@
         	<input type="submit" value="Envoyer" /> 
         </p>    
     </form>
- 
+    <?php
+      try
+       	{    
+       	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;    
+       	$bdd = new PDO('mysql:host=localhost;dbname=test', 'root', '', $pdo_options); 
+ 		$reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 10'); 
+ 		 // Affichage de chaque message (toutes les données sont protégées par htmlspecialchars) 
+ 		 while ($donnees = $reponse->fetch())
+ 		 	 {        
+ 		 	 	echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';    
+ 		 	 }
+ 		 	  $reponse->closeCursor();
+ 		 } 
+ 		 catch(Exception $e) 
+ 		 {    
+ 		 	die('Erreur : '.$e->getMessage()); 
+ 		 }
+
