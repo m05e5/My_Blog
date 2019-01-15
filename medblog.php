e01c1491bcfcc09bbc72db22e111d3c8ae4b6e0d
unknown <calebmoses1111@gmail.com>
2019-01-15 17:34:29 +0100

    comment

--- medblog.php
+++ medblog.php
@@ -26,7 +26,18 @@
        	{    
        	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;    
        	$bdd = new PDO('mysql:host=localhost;dbname=my_blog', 'root', '', $pdo_options); 
- 		$reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 10'); 
+ 		$reponse = $bdd->query('SELECT comment FROM medblog ORDER BY ID DESC LIMIT 0, 10'); 
+ 		 // Affichage de chaque message (toutes les données sont protégées par htmlspecialchars) 
+ 		 while ($donnees = $reponse->fetch())
+ 		 	 {        
+ 		 	 	echo '<p><strong>' . htmlspecialchars($donnees['comment']) . '</strong> : </p>';    
+ 		 	 }
+ 		 	  $reponse->closeCursor();
+ 		 } 
+ 		 catch(Exception $e) 
+ 		 {    
+ 		 	die('Erreur : '.$e->getMessage()); 
+ 		 }
  		?>
 </body>
 </html>
