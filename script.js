b8c1a8431f931eb27e2f4456a15e987fcf27f4f7
unknown <calebmoses1111@gmail.com>
2019-02-14 13:52:32 +0100

    my js test

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function sayhello(){
 	console.log("Hello");
 }
+sayhello();
+
+var sayBye = function() {
+	console.log("Bye");
+}
+
+sayBye();
+
+function sing(song){
+	console.log("AHHHHHHHHH");
+}
+
+sing();
