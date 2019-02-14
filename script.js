2118f3cb1152f7f1c9fbde3f432dab27d2eb4096
unknown <calebmoses1111@gmail.com>
2019-02-14 10:45:25 +0100

    my js test

---
+++ script.js
@@ -0,0 +1,10 @@
+4 + 3;
+
+if (4 + 3 ===7){
+	alert("helloooooooo");
+	console.log("Helloooooo");
+}
+console.log("Helloooooo");
+console.log("Helloooooo");
+console.log("Helloooooo");
+console.log("Helloooooo");
--- scrpt.js
+++
@@ -1,5 +0,0 @@
-4 + 3;
-
-if (4 + 3 ===7){
-	console.log("Helloooooo");
-}
