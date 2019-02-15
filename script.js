cda31d6302aed4a166086913ed74233dd63db171
unknown <calebmoses1111@gmail.com>
2019-02-15 23:47:30 +0100

    mini facebook

--- script.js
+++ script.js
@@ -15,7 +15,16 @@ var newsFeed = [
 		timeline: "javascript is sooooo coool!"
 
 	}
-]:
+]
 
 var userNameprompt = prompt("what's your username");
 var passwordprompt = prompt("what's your password");
+
+function signIn(user, pass) {
+	if(user === database[0].username && pass === database[0].password){
+		console.log(newsFeed);
+	} else {
+		alert("sorry, Wrong username and password!");
+	}
+}
+signIn(userNameprompt, passwordprompt);
