f764b315bce70c123b58b660299fbbeff0f48f40
unknown <calebmoses1111@gmail.com>
2019-02-15 11:06:15 +0100

    I learn arrays in js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-var list = ["tiger", "cat", "bear", "bird"];
-console.log(list)
+var user = {
+	name: "john",
+	age: 34,
+	hobby: false,
+	isMarreied: false,
+	spells: ["abrakadabra", "shazam", "boo"]
+};
+
+var list = [
+	{
+		username: "andy",
+		password: "secre"
+	}
+	{
+		username: "jess",
+		password: "123"
+	}
+];
