17498513b66ea845778b42a545d0d948faae5c85
unknown <calebmoses1111@gmail.com>
2019-02-18 08:36:55 +0100

    simple facebook in js

--- signup.js
+++ signup.js
@@ -2,10 +2,17 @@ var database = [
 	{
 		username: "Kirito",
 		password: "Asuna"
-	}
+	},
+	{
+		username: "sally",
+		password:"123"
+	},
+	{
+		username: "ingrid",
+		password: "777"
 ];
 
-var newsFeed = [
+var newsfeed = [
 	{
 		username: "Bobby",
 		timeline: "so tired from all that learning"
@@ -20,6 +27,13 @@ var newsFeed = [
 		timeline: "javascript is preeetyy cool!"
 	}
 ];
+function signIn(username, password){
+	if (username === database[0].username && password === database[0].password) {
+		console.log(newsfeed);
+	}else{
+
+	}
+}
 
 var userNameprompt = prompt("what's your username");
 var passwordprompt = prompt("what's your password");
