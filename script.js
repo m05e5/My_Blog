7a5506fd282496a343c1d0589f9bfde170719589
unknown <calebmoses1111@gmail.com>
2019-02-23 12:13:13 +0100

    conditions

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-// scope
+//condition ? expr1 : expr2;
 
-var fun = 5;
-
-function funFunction() {
-	// child scope
-	var fun = "hellooo";
-	console.log(1, fun);
-}
-function funerFunction() {
-	// child scope
-	var fun = "Byee";
-	console.log(2, fun);
-}
-function funestFunction() {
-	// child scope
-	var fun = "AHHHHHHHH";
-	console.log(3, fun);
+function isUserValide(bool) {
+	return bool;
 }
 
-console.log("window", fun);
-funFunction();
-funerFunction();
-funestFunction();
+var answer = isUserValide(true) ? "You may enter" : "Access Denied";
