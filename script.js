8a224961ec6e1a000256b05854f7b99ad85021d6
unknown <calebmoses1111@gmail.com>
2019-02-23 12:49:10 +0100

    conditions

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-//condition ? expr1 : expr2;
-
-function isUserValide(bool) {
-	return bool;
+function moveCommand(direction) {
+	var whatHappens;
+	switch(direction){
+		case "forward":
+			whatHappens = "you encounter a monster";
+			break;
+		case "back":
+			whatHappens = "you arrived home";
+			break;
+		case "right":
+			whatHappens = "you found a river";
+			break;
+		case "left":
+			whatHappens = "you run into a troll";
+			break;
+		default:
+			whatHappens = "please enter a valid direction"
+	}
+	return whatHappens;
 }
-
-var answer = isUserValide(true) ? "You may enter" : "Access Denied";
