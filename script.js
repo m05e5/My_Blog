4bf85b474d9e005997a4dc5ee817fed5aafd89bc
unknown <calebmoses1111@gmail.com>
2019-02-26 12:24:30 +0100

    js

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-function moveCommand(direction) {
-	var whatHappens;
-	switch(direction){
-		case "forward":
-			whatHappens = "you encounter a monster";
-			break;
-		case "back":
-			whatHappens = "you arrived home";
-			break;
-		case "right":
-			whatHappens = "you found a river";
-			break;
-		case "left":
-			whatHappens = "you run into a troll";
-			break;
-		default:
-			whatHappens = "please enter a valid direction"
+// function moveCommand(direction) {
+// 	var whatHappens;
+// 	switch(direction){
+// 		case "forward":
+// 			whatHappens = "you encounter a monster";
+// 			break;
+// 		case "back":
+// 			whatHappens = "you arrived home";
+// 			break;
+// 		case "right":
+// 			whatHappens = "you found a river";
+// 			break;
+// 		case "left":
+// 			whatHappens = "you run into a troll";
+// 			break;
+// 		default:
+// 			whatHappens = "please enter a valid direction"
+// 	}
+// 	return whatHappens;
+// }
+ function users () {
+		x = prompt("please enter your name");
 	}
-	return whatHappens;
+	users();
+var x = prompt("please enter the value of x");
+
+function ans(){
+	return 3.14*x*x;
 }
+ans();
